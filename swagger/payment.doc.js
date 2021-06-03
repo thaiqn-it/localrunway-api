9619dcfe08fdc0967ca2f283dfc0f531bb43dab3
hawkeyescodee <liamnguyen123@gmail.com>
2021-06-03 23:43:00 +0700

    ﻿Adding hashtag swagger

--- swagger/payment.doc.js
+++ swagger/payment.doc.js
@@ -1,9 +1,53 @@
+<<<<<<< HEAD
+=======
+//schema
+>>>>>>> 7c802e9 (Adding hashtag swagger)
 /**
  * @swagger
  * components:
  *  schemas:
  *    Payment:
+<<<<<<< HEAD
  *      type: Object
  *      required: true
  *
  */
+=======
+ *      type: string
+ *      required:
+ *        - paymentMethod
+ *      properties:
+ *        id:
+ *          type: string
+ *          description:  The auto generate id of payment
+ *        paymentMethod:
+ *          type: string
+ *          description: Name of the payment
+ *      example:
+ *        name: COD
+ */
+//tag
+/**
+ * @swagger
+ * tags:
+ *  name: Payment
+ *  description: The Payment Management Apis
+ */
+//get
+/**
+ * @swagger
+ * /api/payments:
+ *  get:
+ *    summary: Return list of all payments
+ *    tags: [payments]
+ *    responses:
+ *     200:
+ *      description: The list of all payments
+ *      content:
+ *        application/json:
+ *          schema:
+ *            type: array
+ *            items:
+ *              $ref: '#/components/schemas/Payment'
+ */
+>>>>>>> 7c802e9 (Adding hashtag swagger)
