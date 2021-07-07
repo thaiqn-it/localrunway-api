c07da5463a086a6d674d338db8ee8362ca51c348
nzg76 <khoinguyenit15@gmail.com>
2021-07-08 00:40:05 +0700

    modify

--- constants/index.js
+++ constants/index.js
@@ -1,7 +1,6 @@
 const dotenv = require("dotenv");
 dotenv.config();
-
-exports.APP_PORT = 3000;
+exports.APP_PORT = process.env.PORT || 3000;
 exports.DB_URI = process.env.DB_URI || `mongodb://localhost:27017/localrunway`;
 exports.BCRYPT_SALT_ROUND = 10;
 exports.JWT_SECRET_KEY = "lcrw99";
